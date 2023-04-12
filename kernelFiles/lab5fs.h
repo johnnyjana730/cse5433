657ea29c0783bf5f473275ed2fc7e4ff4a73e856
johnnyjana730 <r04223126@ntu.edu.tw>
2023-04-12 16:26:51 -0400

    'ls'

---
+++ kernelFiles/lab5fs.h
@@ -0,0 +1,74 @@
+#ifndef _LAB5FS_H_
+#define _LAB5FS_H_
+
+#include <linux/types.h>
+#include <linux/fs.h>
+
+/* Definitions */
+#define LAB5FS_SB_MAGIC_NUM  0xCAFECAFE
+#define MAX_FILE_NAME_LEN    21
+#define BLOCK_SIZE 512
+#define NUM_INODES 1000
+#define INODE_SIZE 64
+#define NUM_DATA_BLOCKS 16384
+
+/* Macros */
+#define BYTES_TO_BLOCKS(loc, blocksize) (loc/blocksize)
+#define BLOCK_N(n, blocksize)           (n*blocksize)
+#define INODE_N(n, bs)                  (BLOCK_N(7,BLOCK_SIZE)+(n*bs))
+#define DATA_N(n, bs)                   (BLOCK_N(132,BLOCK_SIZE)+(n*bs))
+
+/* Types */
+typedef struct lab5fs_sb {
+    /* Misc. */
+    unsigned long magic_num;
+
+    /* Sizes */
+    unsigned long blocksize;
+    unsigned long blocksize_bits;
+    unsigned long max_bytes;
+    unsigned long inode_size;
+
+    /* Block locations */
+    unsigned long inode_bitmap_loc;
+    unsigned long data_bitmap_loc;
+    unsigned long inode_loc;
+    unsigned long root_inode_loc;
+    unsigned long data_loc;
+
+    unsigned long last_data_block;
+
+    /* Free/etc. counts */
+    unsigned long data_blocks_free;
+    unsigned long inode_blocks_free;
+    unsigned long data_blocks_total;
+    unsigned long inode_blocks_total;
+
+    __u16 padding[226];
+} lab5fs_sb;
+
+typedef struct lab5fs_ino {
+    /* Inode data */
+    char name[MAX_FILE_NAME_LEN];
+
+    __u32 i_uid;
+    __u32 i_gid;
+    __u32 i_mode;
+    __u16 blocks;
+    __u32 size;
+
+    struct timespec i_atime;
+    struct timespec i_mtime;
+    struct timespec i_ctime;
+
+
+    /* For the data blocks */
+    __u32 i_sblock;
+    __u32 i_eblock;
+
+    /* Link data */
+    __u8 is_hard_link;
+    __u32 block_to_link_to;
+} lab5fs_ino;
+
+#endif
